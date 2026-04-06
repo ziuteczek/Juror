f77db09d6e681b9ab6deb643a7ffeda485155396
Stanislaw Dymny <dymnystan@gmail.com>
2026-04-06 17:25:04 +0200

    unused import removed

--- src/pages/judgement/components/finish.modal.tsx
+++ src/pages/judgement/components/finish.modal.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import flowers from "../../../assets/flowers.svg";
-import { DialogHTMLAttributes, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 export default function FinishModal({
 	albumId,
 	photos,
